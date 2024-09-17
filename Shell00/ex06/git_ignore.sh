82de2cbe866a0b0471f02db958090097d68f3974
JulianaTavs <julianatavaresdasilva@gmail.com>
2024-09-17 13:13:22 -0300

    commit atualizado

--- Shell00/ex06/git_ignore.sh
+++ Shell00/ex06/git_ignore.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-
+#Verifica se estamos em um repositório Git:
+if [ ! -d ../../.git ]; then
+    echo "Este script deve ser executado dentro de um repositório Git."
+fi
 # Lista os arquivos ignorados
 git ls-files --ignored --exclude-standard --others --directory
